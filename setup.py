b2f04fdaa21b6da7b143814e1ba5362c9da7f265
rhnvrm <rohanverma2004@gmail.com>
2015-09-18 21:49:39 +0530

    dont use setup right now its useless

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='baato',
+    name='Baato',
     version='0.0.1',
     packages=[''],
     url='https://github.com/rhnvrm/Baato/',
     license='GNU GPL',
     author='rhnvrm',
     author_email='hello@rohanverma.net',
-    description='HTTP Fileserver with Listing of Local Servers'
+    description='HTTP Fileserver with Listing of Local Servers',
+    install_requires=[
+        "flask"
+    ]
 )
